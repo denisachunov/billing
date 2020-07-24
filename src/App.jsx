d8c18012af6710c02f162b7a3e5e2ea1ffa79a76
Denis-Akhunov <psvsv@yahoo.com>
2020-07-25 00:19:25 +0300

    tiny refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
-// import BillingList from './components/BillingList';
 import AddBilling from './components/AddBilling';
 import BillingsList from './components/BillingsList';
 import BillingContextProvider from './context/BillingsContext';
 import { Header, Container } from 'semantic-ui-react';
-import 'semantic-ui-css/semantic.min.css';
 
 export default () => {
 
